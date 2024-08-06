d6951b278e6233bc7131a63e3da17dd9c8f088a9
Juan Sebastian Ramirez Artiles <juanseraar@hotmail.com>
2024-08-06 08:56:37 +0000

    Check formater

--- android/app/src/main/java/com/totalplaintextreader/MainApplication.kt
+++ android/app/src/main/java/com/totalplaintextreader/MainApplication.kt
@@ -6,54 +6,50 @@ import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.react.uimanager.ViewManager
 import com.facebook.soloader.SoLoader
 
-import com.facebook.react.bridge.NativeModule
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.uimanager.ViewManager
+class MainApplication :
+    Application(),
+    ReactApplication {
+    override val reactNativeHost: ReactNativeHost =
+        object : DefaultReactNativeHost(this) {
+            override fun getPackages(): List<ReactPackage> =
+                PackageList(this).packages.apply {
+                    // Packages that cannot be autolinked yet can be added manually here, for example:
+                    // add(MyReactNativePackage())
+                    add(
+                        object : ReactPackage {
+                            override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> =
+                                listOf(MediaStoreModule(reactContext))
 
-import com.totalplaintextreader.MediaStoreModule
+                            override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> = emptyList()
+                        },
+                    )
+                }
 
-class MainApplication : Application(), ReactApplication {
+            override fun getJSMainModuleName(): String = "index"
 
-  override val reactNativeHost: ReactNativeHost =
-      object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> =
-            PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
-               add(object : ReactPackage {
-                    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
-                        return listOf(MediaStoreModule(reactContext))
-                    }
+            override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-                    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
-                        return emptyList()
-                    }
-                    
-                })
-            }
+            override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+            override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+        }
 
-        override fun getJSMainModuleName(): String = "index"
+    override val reactHost: ReactHost
+        get() = getDefaultReactHost(applicationContext, reactNativeHost)
 
-        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-
-        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-      }
-
-  override val reactHost: ReactHost
-    get() = getDefaultReactHost(applicationContext, reactNativeHost)
-
-  override fun onCreate() {
-    super.onCreate()
-    SoLoader.init(this, false)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this, false)
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            // If you opted-in for the New Architecture, we load the native entry point for this app.
+            load()
+        }
     }
-  }
 }
