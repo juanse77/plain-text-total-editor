807acbfe9aa3eeb7f7e3489202dc02f21f9b4724
Juan Sebastian Ramirez Artiles <juanseraar@hotmail.com>
2024-08-06 19:18:38 +0000

    v1.0.0

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import {
   View,
   Text,
   TextInput,
-  Button,
+  TouchableOpacity,
   Switch,
   StyleSheet,
   Alert,
@@ -11,6 +11,8 @@ import {
 import DocumentPicker from 'react-native-document-picker';
 import RNFS from 'react-native-fs';
 import {NativeModules} from 'react-native';
+
+const { MediaStoreModule } = NativeModules;
 
 const FileEditor = () => {
   const [fileUri, setFileUri] = useState(null);
@@ -55,6 +57,12 @@ const FileEditor = () => {
   }
 
   const saveFileContent = async () => {
+
+    if(fileName === ""){
+        Alert.alert("You must give a file name");
+        return;
+    }
+
     try {
       const result = await NativeModules.MediaStoreModule.saveFile(
         fileName,
@@ -75,7 +83,10 @@ const FileEditor = () => {
 
   return (
     <View style={styles.container}>
-      <Button title="Select File" onPress={openFile} />
+      <TouchableOpacity style={styles.button} onPress={openFile}>
+        <Text style={styles.buttonText}>Select file</Text>
+      </TouchableOpacity>
+
       {fileUri && (
         <>
           <Text style={styles.fileName}>File: {fileName}</Text>
@@ -86,10 +97,24 @@ const FileEditor = () => {
             onChangeText={setFileContent}
           />
           <View style={styles.switchContainer}>
-            <Text>Overwrite File:</Text>
-            <Switch value={overwrite} onValueChange={setOverwrite} />
+            <Text style={styles.label}>Overwrite File: </Text>
+            <Switch
+                value={overwrite}
+                onValueChange={setOverwrite}
+                trackColor={{ false: "#767577", true: "#767577" }}
+                thumbColor={overwrite ? "#FF6347" : "#d2ba32"}
+            />
           </View>
-          <Button title="Save Changes" onPress={saveFileContent} />
+          <Text style={styles.label}>Modify the file name:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={setFileName}
+            value={fileName}
+            placeholder="Enter new name"
+          />
+          <TouchableOpacity style={styles.button} onPress={saveFileContent}>
+            <Text style={styles.buttonText}>Save Changes</Text>
+          </TouchableOpacity>
         </>
       )}
     </View>
@@ -100,28 +125,60 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
+    fontSize: 20,
     justifyContent: 'flex-start',
     alignItems: 'stretch',
     backgroundColor: '#f0f0f0',
   },
   fileName: {
     marginTop: 20,
-    fontSize: 16,
+    fontSize: 20,
     fontWeight: 'bold',
   },
   textInput: {
     flex: 1,
+    fontSize: 16,
     borderColor: '#ccc',
     borderWidth: 1,
     marginTop: 20,
     padding: 10,
     backgroundColor: '#fff',
   },
+  input: {
+    width: '100%',
+    marginBottom: 20,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+    fontSize: 20,
+  },
   switchContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     marginTop: 20,
     marginBottom: 20,
+    justifyContent: 'flex-start',
+  },
+  label: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginBottom: 10,
+    marginTop: 10,
+  },
+  button: {
+    backgroundColor: '#FF6347',  // Un color vistoso, Tomate
+    padding: 12,                 // Espaciado interno para que el botón sea más grande y cómodo
+    borderRadius: 8,             // Bordes redondeados
+    alignItems: 'center',        // Alinea el texto al centro horizontalmente
+    justifyContent: 'center',    // Alinea el texto al centro verticalmente
+    width: '100%',               // Ocupa el 100% del ancho del contenedor padre
+    marginVertical: 5,           // Margen vertical para separar de otros elementos si es necesario
+  },
+  buttonText: {
+    fontSize: 20,                // Tamaño de texto
+    color: 'white',              // Color del texto
+    textTransform: 'uppercase',  // Texto en mayúsculas
   },
 });
 
