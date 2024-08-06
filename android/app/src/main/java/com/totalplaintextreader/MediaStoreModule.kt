55538ffd1ea1fe640343aceb41ce89454401dd18
Juan Sebastian Ramirez Artiles <juanseraar@hotmail.com>
2024-08-06 10:28:15 +0000

    Add autorename file

--- android/app/src/main/java/com/totalplaintextreader/MediaStoreModule.kt
+++ android/app/src/main/java/com/totalplaintextreader/MediaStoreModule.kt
@@ -1,22 +1,22 @@
 package com.totalplaintextreader
 
+import android.content.ContentUris
 import android.content.ContentValues
-import android.content.Context
 import android.net.Uri
 import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
+import com.facebook.react.bridge.Callback
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Callback
-import java.io.OutputStream
 
-class MediaStoreModule(private val reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+class MediaStoreModule(
+    private val reactContext: ReactApplicationContext,
+) : ReactContextBaseJavaModule(reactContext) {
     private val BUFFER_SIZE = 1024
 
-    override fun getName(): String {
-        return "MediaStoreModule"
-    }
+    override fun getName(): String = "MediaStoreModule"
 
     private fun getMimeTypeFromExtension(fileName: String): String {
         val extension = fileName.substringAfterLast('.', "")
@@ -38,32 +38,76 @@ class MediaStoreModule(private val reactContext: ReactApplicationContext) : Reac
     }
 
     @ReactMethod
-    fun saveFile(fileName: String, fileContent: String, callback: Callback) {
+    fun saveFile(
+        fileName: String,
+        fileContent: String,
+        shouldOverwrite: Boolean,
+        callback: Callback,
+    ) {
         try {
-            // Crear y obtener el URI del archivo
-            val values = ContentValues().apply {
-                put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
-                put(MediaStore.MediaColumns.MIME_TYPE, getMimeTypeFromExtension(fileName))
-                put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS)
-            }
-            val uri: Uri? = reactContext.contentResolver.insert(MediaStore.Files.getContentUri("external"), values)
-            
-            if (uri == null) {
-                callback.invoke("Failed to create a new MediaStore record.", null)
-                return
-            }
+            val fileUri = checkIfFileExists(fileName)
 
-            // Escribir contenido en el archivo
-            reactContext.contentResolver.openOutputStream(uri)?.use { outputStream ->
-                outputStream.writer().use { writer ->
-                    writer.write(fileContent)
+            if (fileUri != null && !shouldOverwrite) {
+                writeContentToFile(fileUri, fileContent)
+                callback.invoke(null, "File saved successfully.")
+            } else {
+                if (fileUri != null) {
+                    reactContext.contentResolver.delete(fileUri, null, null) // Delete existing file
                 }
+                val uri = createFileUri(fileName)
+                writeContentToFile(uri, fileContent)
+                callback.invoke(null, "File saved successfully.")
             }
-
-            callback.invoke(null, "File saved successfully.")
         } catch (e: Exception) {
             callback.invoke(e.toString(), null)
         }
     }
 
+    private fun checkIfFileExists(fileName: String): Uri? {
+        val fileCursor =
+            reactContext.contentResolver.query(
+                MediaStore.Files.getContentUri("external"),
+                arrayOf(MediaStore.MediaColumns._ID),
+                "${MediaStore.MediaColumns.DISPLAY_NAME} = ?",
+                arrayOf(fileName),
+                null,
+            )
+        fileCursor?.use { cursor ->
+            if (cursor.moveToFirst()) {
+                val columnIndex = cursor.getColumnIndex(MediaStore.MediaColumns._ID)
+                if (columnIndex != -1) {
+                    val id = cursor.getLong(columnIndex)
+                    return ContentUris.withAppendedId(MediaStore.Files.getContentUri("external"), id)
+                } else {
+                    Log.e("MediaStoreModule", "Column ID not found")
+                }
+            }
+        }
+        return null
+    }
+
+    private fun createFileUri(fileName: String): Uri? {
+        val values =
+            ContentValues().apply {
+                put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
+                put(MediaStore.MediaColumns.MIME_TYPE, getMimeTypeFromExtension(fileName))
+                put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS)
+            }
+        return reactContext.contentResolver.insert(MediaStore.Files.getContentUri("external"), values)
+    }
+
+    private fun writeContentToFile(
+        uri: Uri?,
+        content: String,
+    ) {
+        uri?.let {
+            reactContext.contentResolver.openOutputStream(it)?.use { outputStream ->
+                outputStream.bufferedWriter().use { writer ->
+                    writer.write(content)
+                }
+            }
+        } ?: run {
+            Log.e("MediaStoreModule", "Failed to get URI for writing content.")
+        }
+    }
 }
