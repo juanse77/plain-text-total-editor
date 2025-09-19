a557d20b320bab91d0a05125112df71a24f20aef
Juan Sebastián Ramírez Artiles <juanseraar@hotmail.com>
2025-09-19 22:06:56 +0100

    Update compiler

--- android/app/src/main/java/com/totalplaintextreader/MainApplication.kt
+++ android/app/src/main/java/com/totalplaintextreader/MainApplication.kt
@@ -3,56 +3,52 @@ package com.totalplaintextreader
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
-import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.uimanager.ViewManager
 import com.facebook.soloader.SoLoader
 
-class MainApplication :
-    Application(),
-    ReactApplication {
-    override val reactNativeHost: ReactNativeHost =
-        object : DefaultReactNativeHost(this) {
-            override fun getPackages(): List<ReactPackage> =
-                PackageList(this).packages.apply {
-                    // Packages that cannot be autolinked yet can be added manually here, for example:
-                    // add(MyReactNativePackage())
-                    add(
-                        object : ReactPackage {
-                            override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> =
-                                listOf(
-                                    MediaStoreModule(reactContext),
-                                    FileIntentModule(reactContext)
-                                )
+class MainApplication : Application(), ReactApplication {
 
-                            override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> = emptyList()
-                        },
-                    )
-                }
+  override val reactNativeHost: ReactNativeHost =
+    object : DefaultReactNativeHost(this) {
 
-            override fun getJSMainModuleName(): String = "index"
+      override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-            override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+      override fun getPackages(): List<ReactPackage> {
+        val packages = PackageList(this).packages.toMutableList()
+        // Añade aquí tus paquetes manuales:
+        packages.add(
+          object : ReactPackage {
+            override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> =
+              listOf(
+                MediaStoreModule(reactContext),
+                FileIntentModule(reactContext)
+              )
 
-            override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-            override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-        }
+            override fun createViewManagers(
+              reactContext: ReactApplicationContext
+            ): List<ViewManager<*, *>> = emptyList()
+          }
+        )
+        return packages
+      }
 
-    override val reactHost: ReactHost
-        get() = getDefaultReactHost(applicationContext, reactNativeHost)
+      override fun getJSMainModuleName(): String = "index"
 
-    override fun onCreate() {
-        super.onCreate()
-        SoLoader.init(this, false)
-        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            // If you opted-in for the New Architecture, we load the native entry point for this app.
-            load()
-        }
+      override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+      override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
     }
+
+  override fun onCreate() {
+    super.onCreate()
+    SoLoader.init(this, false)
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      DefaultNewArchitectureEntryPoint.load()
+    }
+  }
 }
